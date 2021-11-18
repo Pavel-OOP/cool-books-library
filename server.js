d9b2d8334f679671edf50679d5ea6b1966d14beb
Pavel-OOP <pavel.i.petrov.7@gmail.com>
2021-11-18 12:10:49 +0200

    cmon heroku WORK!

--- server.js
+++ server.js
@@ -20,4 +20,6 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
