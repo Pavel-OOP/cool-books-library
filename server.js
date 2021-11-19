f63d08f1d8e6a8877baa158b6b48e311181eca51
Pavel-OOP <pavel.i.petrov.7@gmail.com>
2021-11-19 16:29:49 +0200

    Search parameters

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
-app.use('/authors', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
